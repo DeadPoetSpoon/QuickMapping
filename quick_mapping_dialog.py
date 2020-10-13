168acf574148a1e796d608df951a2f2d9bcb8e3b
DeadPoetSpoon <1149097040@qq.com>
2020-10-13 20:19:41 +0800

    作业完成,但是插件之路漫漫

--- quick_mapping_dialog.py
+++ quick_mapping_dialog.py
@@ -52,7 +52,8 @@ class QuickMappingDialog(QtWidgets.QDialog, FORM_CLASS):
         self.iface = iface
         self.layer = None
         self.joinlayer = None
-        self.categorizedSRWidget = None
+        self.layout = None
+        # self.categorizedSRWidget = None
         # ============================================================
         # connet
         # loadmap
@@ -65,7 +66,22 @@ class QuickMappingDialog(QtWidgets.QDialog, FORM_CLASS):
                 "/home",
                 "table(*.csv *.xlsx)")[0]))
         # style
-        self.pb_single.clicked.connect(self.singleSymbol)
+        self.rendererDlg = None
+        self.pb_symbolize.clicked.connect(self.symbolize)
+        # self.pb_single.clicked.connect(self.singleSymbol)
+        # self.pb_graduated.clicked.connect(self.graduatedSymbol)
+        # map opt
+        self.pb_setCRS.clicked.connect(self.setCRS)
+        # labeling map
+        self.pb_labeling.clicked.connect(self.labeling)
+        # ExportD
+        self.pb_exportD.clicked.connect(lambda:
+                self.exportD(QFileDialog.getSaveFileName(self,
+                "Choose Save File Name",
+                "/home",
+                "(*)")[0]))
+        # layout
+        self.pb_createlayout.clicked.connect(self.printlayout)
         # =============================================================
         # load AreaCode
         self.dir = os.path.dirname(__file__)
@@ -156,39 +172,143 @@ class QuickMappingDialog(QtWidgets.QDialog, FORM_CLASS):
         # Get Json
         dataurl = self.getUrl(area)
         if dataurl != "":
-            self.iface.newProject(True)
+            if not self.iface.newProject(True):
+                self.showError("需要创建新工程~")
+                return
             self.layer = self.iface.addVectorLayer(dataurl, area, "ogr")
+            # 标记标记字段
+            for i in range(self.cbb_labelingfield.count()-1, -1, -1):
+                self.cbb_labelingfield.removeItem(i)
+            for col in self.layer.fields():
+                self.cbb_labelingfield.addItem(col.name())
+            # 设置符号化
+            if self.rendererDlg is not None:
+                del self.rendererDlg
+                self.rendererDlg = None
+            self.rendererDlg = QgsRendererPropertiesDialog(self.layer,QgsStyle(),True,self)
+            self.sa_symbolize.setWidget(self.rendererDlg)
         else:
             self.showError("地区不存在，或地区名不完整。")
-    def singleSymbol(self):
+    # ==============================================================
+    # 符号化
+    def symbolize(self):
+        """apply symbol"""
         if self.layer is None:
-            self.showError("请先加载地图！")
+            self.showError("请先加载地图!")
             return
-        # 选择颜色
-        colorDlg = QgsColorDialog(self)
-        colorDlg.show()
-        colorDlg.exec_()
-        color = colorDlg.color().getRgbF()
-        renderer = self.layer.renderer()
-        symbol = QgsFillSymbol.createSimple({'color': color})
-        self.layer.renderer().setSymbol(symbol)
+        self.rendererDlg.apply()
         self.layer.triggerRepaint()
-        
-    def applySymbol(self):
-        self.categorizedSRWidget.renderer().startRender()
-        self.categorizedSRWidget.applyChangeToSymbol()
-        self.categorizedSRWidget.applyChanges()
-        self.categorizedSRWidget.renderer().stopRender()
+    # wo shi sha bi
+    # def singleSymbol(self):
+    #     if self.layer is None:
+    #         self.showError("请先加载地图！")
+    #         return
+    #     # 选择颜色
+    #     colorDlg = QgsColorDialog(self)
+    #     colorDlg.show()
+    #     colorDlg.exec_()
+    #     color = colorDlg.color().getRgb()
+    #     # self.showLog(color)
+    #     renderer = self.layer.renderer()
+    #     # self.showLog(str(color).replace('(','').replace(')',''))
+    #     symbol = QgsFillSymbol.createSimple({'color':str(color).replace('(','').replace(')','')})
+    #     renderer.setSymbol(symbol)
+    #     self.layer.triggerRepaint()
+    #     self.showLog(self.layer.renderer().symbol().symbolLayers()[0].properties())
+    #     # def graduatedSymbol(self):
+    #     dlg = QgsRendererPropertiesDialog(self.layer,QgsStyle(),true,self)
+    #     dlg.show()
+    #     dlg.exec_()
+    #     graduatedRenderer = QgsGraduatedSymbolRenderer()
+    #     self.layer.setRenderer(QgsGraduatedSymbolRenderer())
+    #     self.layer.triggerRepaint()
+    # ===========================================================
+    def exportD(self,filepath):
+        """save map"""
+        save_options = QgsVectorFileWriter.SaveVectorOptions()
+        save_options.driverName = "ESRI Shapefile"
+        save_options.fileEncoding = "UTF-8"
+        transform_context = QgsProject.instance().transformContext()
+        error = QgsVectorFileWriter.writeAsVectorFormatV2(self.layer,
+                                                        filepath,
+                                                        transform_context,
+                                                        save_options)
+        if error[0] == QgsVectorFileWriter.NoError:
+            self.showMsg("导出成功!")
+        else:
+            self.showError(error)
+    # ===========================================================
+    def printlayout(self):
+        """layout"""
+        if self.layer is None:
+            self.showError("请先加载地图!")
+            return
+        if self.le_layoutname.text() == '':
+            self.showError("请输入打印布局名")
+            return
+        if self.layout is None:
+            self.layout = QgsPrintLayout(QgsProject.instance())
+            self.layout.initializeDefaults()
+            self.layout.setName(self.le_layoutname.text())
+        # can not add right map QAQ
+        # mmap = QgsLayoutItemMap(self.layout)
+        # mmap.setLayers([self.layer])
+        # mmap.setExtent(self.layer.extent())
+        # mmap.setCrs(self.layer.sourceCrs())
+        # mmap.attemptMove(QgsLayoutPoint(6, 10, QgsUnitTypes.LayoutMillimeters))
+        # mmap.attemptResize(QgsLayoutSize(90, 80, QgsUnitTypes.LayoutMillimeters))
+        # self.layout.addLayoutItem(mmap)
+        if self.cb_mapname.isChecked():
+            label = QgsLayoutItemLabel(self.layout)
+            label.setText(self.le_mapname.text())
+            label.adjustSizeToText()
+            self.layout.addLayoutItem(label)
+        if self.cb_scale.isChecked():
+            item = QgsLayoutItemScaleBar(self.layout)
+            item.setStyle('Numeric')
+            item.setLinkedMap(mmap) 
+            item.applyDefaultSize()
+            self.layout.addLayoutItem(item)
+        if self.cb_legend.isChecked():
+            legend = QgsLayoutItemLegend(self.layout)
+            legend.setLinkedMap(mmap)
+            self.layout.addLayoutItem(legend)
+        layoutManager = QgsProject.instance().layoutManager()
+        if layoutManager.layoutByName(self.layout.name()) is None:
+            layoutManager.addLayout(self.layout)
+        self.iface.openLayoutDesigner(self.layout)
+    # ===========================================================
+    def labeling(self):
+        """labeling layer"""
+        if self.layer is None:
+            self.showError("请先加载地图!")
+            return
+        pal_layer = QgsPalLayerSettings()
+        pal_layer.fieldName = self.cbb_labelingfield.currentText()
+        pal_layer.enabled = True
+        pal_layer.placement = QgsPalLayerSettings.OverPoint
+        labels = QgsVectorLayerSimpleLabeling(pal_layer)
+        self.layer.setLabeling(labels)
+        self.layer.setLabelsEnabled(True)
         self.layer.triggerRepaint()
     # ===========================================================
-    def showError(self, msg: str):
+    def setCRS(self):
+        """Set CRS"""
+        if self.layer is None:
+            self.showError("请先加载地图!")
+            return
+        QgsProject.instance().setCrs(QgsCoordinateReferenceSystem(self.le_CRS.text()))
+        self.layer.triggerRepaint()
+        self.iface.zoomFull()
+    # ===========================================================
+    def showError(self, msg):
         """show error dialog"""
         # QgsErrorDialog.show(
         #     QgsError(msg, "Quick Mapping"), "Error")
-        self.msgBar.pushWarning("Quick Mapping",msg)
-    def showMsg(self, msg: str):
+        self.msgBar.pushWarning("Quick Mapping",str(msg))
+    def showMsg(self, msg):
         """show msg"""
-        self.msgBar.pushSuccess("Quick Mapping",msg)
-    def showLog(self, log: str):
+        self.msgBar.pushSuccess("Quick Mapping",str(msg))
+    def showLog(self, log):
         """show log"""
-        QgsMessageLog.logMessage(log, 'Quick Mapping', level=Qgis.Info)
+        QgsMessageLog.logMessage(str(log), 'Quick Mapping', level=Qgis.Info)
